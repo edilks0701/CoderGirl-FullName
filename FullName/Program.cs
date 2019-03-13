750b8daec4bc19bb9ffec610e01e37d97d05256d
Jonny McFadden <jmcfadden1528@gmail.com>
2019-03-13 16:09:58 -0500

    Let's try this agaaaaain

--- FullName/Program.cs
+++ FullName/Program.cs
@@ -6,7 +6,7 @@ namespace FullName
     {
         public static void Main()
         {
-            // TODO: Ask the user for their first name and then their last name.
+         
             string firstName;
             Console.WriteLine("What is your first name?");
             firstName = Console.ReadLine();
@@ -17,22 +17,15 @@ namespace FullName
 
             FullName(firstName,lastName);
 
-              Console.ReadLine();
+            Console.ReadLine();
         }
 
-        public static void FullName(2)
+        public static void FullName(string firstName, string lastName)
         {
-            //string firstname;
-            //Console.WriteLine("What is your first name?");
-            //firstname = Console.ReadLine();
-
-            //string lastName;
-            //Console.WriteLine("What is your last name?");
-            //lastName = Console.ReadLine();
-            Console.WriteLine(firstName + " " + lastName);
             
-            
+            string fullName = firstName + " " + lastName;
 
+            return FullName;
         }
 
 
